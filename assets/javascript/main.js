40b525e7ad83dbbc34da300d65be59637fdb06b0
Salomao Rodrigues <salomao@Salomaos-MacBook-Pro.local>
2015-12-14 10:52:20 +0000

    Add ace editor to project

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,12 +1,25 @@
 /*jshint esnext: true */
-(function scaleAllSteps() {
+(function () {
     "use strict";
+    let scaleAllSteps = function () {
+        const SCALE = "0.1";
 
-    const SCALE = "0.1";
+        let scaled = document.querySelectorAll(".scaled");
 
-    let scaled = document.querySelectorAll(".scaled");
+        Array.from(scaled).forEach(function (element) {
+            element.setAttribute("data-scale", SCALE);
+        });
+    };
 
-    Array.from(scaled).forEach(function (element) {
-        element.setAttribute("data-scale", SCALE);
-    });
+    let runLetCode = function () {
+        let code = editor.getSession().getValue();
+        eval(code);
+    };
+
+    let init = function init() {
+        scaleAllSteps();
+        document.querySelector(".run-let-code").addEventListener("click", runLetCode);
+    };
+
+    init();
 }());
